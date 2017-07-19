7733d7dc6feddaba2d109719fbc8576216a1f52a
GQxQD <916309979@qq.com>
2017-07-19 15:20:28 +0800

    sy
    sy

--- ssm20170718/school/src/cn/gqxqd/controller/SchoolController.java
+++ ssm20170718/school/src/cn/gqxqd/controller/SchoolController.java
@@ -1,5 +1,8 @@
 package cn.gqxqd.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/school")
 public class SchoolController {
