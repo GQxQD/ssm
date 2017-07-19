e04f636f8a05e211da8bf3fc066da213ae48b128
GQxQD <916309979@qq.com>
2017-07-19 17:01:33 +0800

    sys
    sys

--- ssm20170718/school/src/cn/gqxqd/controller/SchoolController.java
+++ ssm20170718/school/src/cn/gqxqd/controller/SchoolController.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/school")
+@RequestMapping("school")
 public class SchoolController {
 
+	@RequestMapping("list")
+	public String gotopage(){
+		return "hui/index";
+	}
 }
